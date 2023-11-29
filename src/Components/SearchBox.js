48ea8e136e16916789474e147a2f55163dd17629
Jacqueline Wettergren <jacqueline.wettergren@gmail.com>
2023-11-29 17:13:25 +0100

    231129

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -7,6 +7,7 @@ const Searcbox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Search Robots"
         className="bg-light-blue dib tc br3 pa3 ma2 grow"
         type="search"
         placeholder="search robots"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import './index.css';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers} from 'redux';
 import App from './Containers/App';
-import reportWebVitals from './reportWebVitals';
+;
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
-import {createLogger} from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const logger = createLogger({});
@@ -24,4 +25,10 @@ root.render(
    <Provider store={store}><App/> </Provider> 
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 reportWebVitals();
